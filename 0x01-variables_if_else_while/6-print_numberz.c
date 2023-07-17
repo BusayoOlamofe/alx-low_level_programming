8aea6d7d1769f6782fad37f980378af88b14a9c1
Busayo Olamofe <busayoadeleke8@gmail.com>
2023-07-17 15:19:47 -0700

    print numbers

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -2,6 +2,13 @@
 #include <time.h>
 #include <stdio.h>
 
+/**
+* Main: Entry point
+* Description- prints all single digit numbers of base 10 starting from 0, followed by a new line
+* Parameter: use only putchar function
+* Return: 0(if successful)
+*/
+
 int main(void)
 {
 	int i;
@@ -10,5 +17,5 @@ int main(void)
 	putchar(i);
 	putchar('\n');
 
-return (0);
+	return (0);
 }
