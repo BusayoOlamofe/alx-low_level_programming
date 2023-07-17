defd69c5cc3a4c50c7ef30c1bc9f6a7aeef6c3cf
Busayo Olamofe <busayoadeleke8@gmail.com>
2023-07-17 08:29:50 -0700

    print char reverse

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+*main-Entry point
+*Description- print alphabet in reverse
+*Return: 0(If successful)
+*/
+
+int main(void)
+{
+ char rev;
+
+	for (rev = 'z'; rev >= 'a'; rev--)
+	{	
+	putchar(rev);
+	}
+	putchar('\n');
+
+	return (0);
+}
