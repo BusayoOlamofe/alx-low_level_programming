829ab07a64886fac38339084e968dd7ca7f66bf2
Busayo Olamofe <busayoadeleke8@gmail.com>
2023-07-17 15:57:30 -0700

    print alphabet in lowercase

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -3,7 +3,12 @@
 #include <time.h>
 /* more headers goes there */
 
-/* betty style doc for function main goes there */
+/**
+*main-Entry point
+*Description- print alphabet in reverse
+*Return: 0(If successful)
+*/
+
 int main(void)
 {
 	char low;
