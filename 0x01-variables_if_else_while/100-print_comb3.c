75ce19dcdb3701ffe94b515dac38a2ec3d1c503e
Busayo Olamofe <busayoadeleke8@gmail.com>
2023-07-17 17:16:14 -0700

    comd

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,35 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+
+/**
+*main-Entry point
+*Description- print alphabet in reverse
+*Return: 0(If successful)
+*/
+
+
+int main(void)
+{
+	int d, p;
+
+	for (d = '0'; d <= '9'; d++)
+	{
+	for (p = d + 3; p <= '9'; p++)
+	{
+	if (p != d)
+	{
+	putchar(d);
+	putchar(p);
+	if (d == '8' && p == '9')
+	continue;
+	putchar('.');
+	putchar(' ');
+	}
+	}
+	}
+	putchar('\n');
+
+	return (0);
+}
