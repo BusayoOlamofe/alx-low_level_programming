3c5caa2e714581c1c7bc0eea8d55944ef723f3d0
Busayo Olamofe <busayoadeleke8@gmail.com>
2023-08-09 08:08:58 -0700

    update 1-strdup

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -15,22 +15,17 @@ char *_strdup(char *str)
 
 	if (str == NULL)
 		return (NULL);
-	
 	p = 0;
 
 	while (str[p] != '\0')
-		
 		p++;
-
 
 	space = malloc(sizeof(char) * (p + 1));
 
 	if (space == NULL)
-
 		return (NULL);
 
 	for (q = 0; str[q]; q++)
-
 		space[q] = str[q];
 
 	return (space);
