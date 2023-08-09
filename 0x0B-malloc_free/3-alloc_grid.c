8ae020854b659979aaaf1449f6a49a901a21ef9b
Busayo Olamofe <busayoadeleke8@gmail.com>
2023-08-09 08:42:16 -0700

    update: 3-allloc_grid.c

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -2,11 +2,10 @@
 #include <stdlib.h>
 
 /**
- * create_array - create array of size size and assign char c
- * @size: size of array
- * @c: char to assign
- * Description: 
- * Return: concat s1 and s2
+ * alloc_grid - create two dimensional array of integer
+ * @width: integer
+ * @height: integer
+ * Return: pointer to array or NULL on failure
  *
  */
 
@@ -16,37 +15,32 @@ int **alloc_grid(int width, int height)
 	int h = 0;
 	int **point;
 
-
-	if(width <= 0 || height <= 0)
-
+	if (width <= 0 || height <= 0)
 		return (NULL);
-	
+
 	point = malloc(sizeof(int *) * height);
 
-	if(point == NULL)
-
+	if (point == NULL)
 		return (NULL);
 
-	for(w = 0; w < height; w++)
-
+	for (w = 0; w < height; w++)
+	{
 		point[w] = malloc(sizeof(int) * width);
 
+		if (point[w] == (NULL))
+		{
+			for (; h >= 0; h--)
+			{
+				free(point[w]);
+			}
 
-	if(point[w] == (NULL))
-
-	{
-	for(; h >= 0; h--)
-	{
-		free(point[w]);
-	}
-		free(point);
-		return (NULL);
-	}
-	for(w = 0; w < height; w++)
+			free(point);
+			return (NULL);
+		}
 
 		for (h = 0; h < width; h++)
 			point[w][h] = 0;
-
+	}
 	return (point);
 }
 
